06265c36804f1e58e5665ecbf62a394e16452d43
Onwuka Peter .E <onwukaemeka41@gmail.com>
2022-09-20 21:47:42 +0100

    v

--- SEC44NIPSS/Areas/ResolvedAdmin/Pages/ParlyDocument/Edit.cshtml.cs
+++ SEC44NIPSS/Areas/ResolvedAdmin/Pages/ParlyDocument/Edit.cshtml.cs
@@ -40,7 +40,7 @@ namespace SEC44NIPSS.Areas.ResolvedAdmin.Pages.ParlyDocument
             }
            ViewData["ParlyReportSubCategoryId"] = new SelectList(_context.ParlyReportSubCategories, "Id", "Title");
            ViewData["ParlyReportCategoryId"] = new SelectList(_context.ParlyReportCategories, "Id", "Title");
-           ViewData["ProfileId"] = new SelectList(_context.Profiles, "Id", "FulName");
+           ViewData["ProfileId"] = new SelectList(_context.Profiles, "Id", "Id");
             return Page();
         }
 
